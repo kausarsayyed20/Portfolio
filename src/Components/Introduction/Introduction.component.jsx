b919b2874fc5c11ed2f1808eea846113de7710e2
kausarsayyed20 <kausarsayyed.20@gmail.com>
2021-07-31 02:13:17 +0530

    Created introduction page

--- src/Components/Introduction/Introduction.component.jsx
+++ src/Components/Introduction/Introduction.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./Introduction.css";
-import image from "../../assets/13264.jpg";
 
+
+import girl from "../../assets/image2vector.svg";
+import { Container, Row, Col, Button } from "react-bootstrap";
 const Introduction = () => {
     return (
         <div className="main-container" style={{
@@ -9,19 +11,38 @@ const Introduction = () => {
             justifyContent: "center",
             alignItems: "center"
         }}>
-            <div className="profile-container" >
-                <h1>Hey I'm Kausar S</h1>
-                <div>
-                    <p>
-                        A passionate Frontend Software Developer 🚀 having an experience of building Web applications with
-                        JavaScript / Reactjs / Nodejs.
-                    </p>
-                </div>
-            </div>
-            <div className="image-container">
-                <img src={image} width="500px" height="500px" alt="kausar" />
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="profile-container" >
+                            <h1><b>Hi, I'm Kausar</b>
 
+                                <span className="hand" role="img" aria-label="Hand waving">
+                                    👋
+                                </span>
+
+                            </h1>
+
+                            <div>
+                                <p>
+                                    A passionate Frontend Software Developer 🚀 having an experience of building Web applications with
+                                    JavaScript / Reactjs / Nodejs.
+                                </p>
+                            </div>
+                            <div className="button-container">
+                                <Button variant="outline-light">CONTACT ME!!</Button>{' '}
+                            </div>
+
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="image-container">
+                            <img src={girl} className="responsive_image img2" width="500vh" height="500vh" alt="kausar" />
+                        </div>
+
+                    </Col>
+                </Row>
+            </Container>
 
         </div>
     )
